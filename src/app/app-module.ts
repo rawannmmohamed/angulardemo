12ba0080a271618706783ae65e7c7006a65cafab
rawannmmohamed <rawanmelkhodary@gmail.com>
2026-01-09 17:55:56 +0200

    init commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Products } from './products/products';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    Footer,
+    Products
   ],
   imports: [
     BrowserModule,
